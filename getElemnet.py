26790f4188320da7279deae868c0dfaa97950a94
roisel <rensirui@gmail.com>
2013-11-04 20:28:38 +0800

    change the date element

--- getElemnet.py
+++ getElemnet.py
@@ -13,23 +13,23 @@ def getdateElement():
     driver = webdriver.Firefox()
     driver.get('http://go.qq.com/v2/flight/list.html?tripType=0&fromCity=PEK&toCity=SHA&depDate=20131104')
     for i in range(1, 8):
-        dateElemnet = "//*[@id='daysLowest']/li[%s]/a/strong" % i
+        dateElemnet = "//*[@class='ticket_main_select']/div[1]/ul[1]/li[%s]/a/strong" % i
         driver.find_element_by_xpath(dateElemnet).click()
-        driver.find_element_by_xpath("//*[@id='flightListTable']/tbody/tr[1]/td[6]/a/span").click()
+        #driver.find_element_by_xpath("//*[@class='ticket_main_select']/div[1]/ul[1]/li[%s]/a/strong").click()
         print 1
         if i == 7:
-            startTime = driver.find_element_by_xpath("//*[@id='daysLowest']/li[4]/a/strong").get_attribute('id')
+            startTime = driver.find_element_by_xpath("//*[@class='ticket_main_select']/div[1]/ul[1]/li[4]/a/strong").get_attribute('id')
             print startTime[-8:]
         else:
             startTime = driver.find_element_by_xpath(dateElemnet).get_attribute('id')
             print startTime[-8:]
     for i in range(5, 8):
-        dateElemnet = "//*[@id='daysLowest']/li[%s]/a/strong" % i
+        dateElemnet = "//*[@class='ticket_main_select']/div[1]/ul[1]/li[%s]/a/strong" % i
         driver.find_element_by_xpath(dateElemnet).click()
         time.sleep(3)
         print 1
         if i == 7:
-            startTime = driver.find_element_by_xpath("//*[@id='daysLowest']/li[4]/a/strong").get_attribute('id')
+            startTime = driver.find_element_by_xpath("//*[@class='ticket_main_select']/div[1]/ul[1]/li[4]/a/strong").get_attribute('id')
             print startTime[-8:]
         else:
             startTime = driver.find_element_by_xpath(dateElemnet).get_attribute('id')
