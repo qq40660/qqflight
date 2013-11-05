0f951b5594e290fd69619fa29c571b85778a1916
roisel <rensirui@gmail.com>
2013-11-05 16:22:20 +0800

    add mail

---
+++ config.py
@@ -0,0 +1,9 @@
+#coding: utf-8
+
+MAIL_HOST = 'smtp.qq.com'
+MAIL_PORT = 25
+MAIL_USERNAME = '35318717@qq.com'
+MAIL_PASSWORD = '3218818ice'
+MAIL_TO = ['35318717@qq.com', '304858826@qq.com']
+
+title = 'hello'
--- tmp/tmpScript.py
+++ tmp/tmpScript.py
@@ -1,79 +1,40 @@
-#-*- coding:utf8 -*-
+ # -*- coding: utf-8 -*-
 
-import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+import smtplib
 
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-url = 'http://go.qq.com/v2/flight/list.html?tripType=0&fromCity=PEK&toCity=XMN&depDate=20131105&qf=go.qq.com%2Fflight%2Fl'
+from config import *
 
-driver = webdriver.Firefox()
-driver.get(url)
+html_1 = '''
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Error Warring</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+<div id="title">
+    <h2>QQ Flight Auto_test Error or Warring Report</h2>
+</div>
+<div>
+    <p>关键字 “酷讯担保”没有出现在结果中</p>
+    <p>城市： %s</p>
+    <p>航班日期： %s</p>
+    <p>航班号： %s</p>
+</div>
+</body>
+</html>
+'''
 
-dateElemnet = "//*[@class='ticket_main_select']/div[1]/ul[1]/li[1]/a/strong"
-driver.find_element_by_xpath(dateElemnet).click()
+smtp = smtplib.SMTP()
+smtp.connect(MAIL_HOST, MAIL_PORT)
+smtp.login(MAIL_USERNAME, MAIL_PASSWORD)
 
-driver.find_element_by_xpath("//*[@id='flightListTable']/tbody/tr[1]/td[6]/a/span").click()
-
-try:
-    webdriver.assertEqual(u"很抱歉，没有找到符合您查询条件的航班", driver.find_element_by_css_selector("p.single_row").text)
-except AssertionError as e:
-    webdriver.verificationErrors.append(str(e))
-
-#driver.find_element_by_xpath(".//*[@id='flightListTable']/tbody/tr[2]/td/div[1]/div[2]/table/tbody")
-#得到下拉报价区的ota数量
-"""
-x = driver.find_element_by_xpath(".//*[@id='flightListTable']/tbody/tr[2]/td/div[1]/div[2]/table/tbody/tr[1]/td[2]").text[:4]
-print x
-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-import unittest, time, re
-
-class Untitled(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
-        self.base_url = "http://go.qq.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
-
-    def test_untitled(self):
-
-        driver = self.driver
-        driver.get(self.base_url + "/v2/flight/list.html?tripType=0&fromCity=PEK&toCity=XMN&depDate=20131105&qf=go.qq.com%2Fflight%2F")
-        try: self.assertTrue(self.is_element_present(By.CSS_SELECTOR, "p.single_row"))
-        except AssertionError as e: self.verificationErrors.append(str(e))
-
-    def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException, e: return False
-        return True
-
-    def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException, e: return False
-        return True
-
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-
-    def tearDown(self):
-        self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
-
-if __name__ == "__main__":
-    unittest.main()
-
-"""
+msg = MIMEText(html_1, 'html', 'utf-8')
+msg['From'] = MAIL_USERNAME
+msg['To'] = '35318718'
+msg['Subject'] = title
+smtp.sendmail(MAIL_USERNAME, '35318717@qq.com', msg.as_string())
+smtp.quit()
